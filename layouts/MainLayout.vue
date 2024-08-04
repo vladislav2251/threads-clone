154b925842546f0f75668feede646ce851ca39ec
vladislav2251 <vladdidyk1985@gmail.com>
2024-08-04 11:42:28 +0300

    frontend

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="MainLayout" class="h-screen">
+        <div class="w-full max-w-[500px] mx-auto">
+            <div id="TopMenu" class="w-full flex items-center justify-center px-2 h-10 my-2">
+                <img class="w-[35px]" src="/public/threads-logo.png" alt="Threads Logo Icon">
+            </div>
+        </div>
+
+        <div class="flex w-full max-w-[500px] mx-auto h-screen">
+            <slot />
+        </div>
+
+        <div
+            id="BottomNav"
+            class="fixed z-50 bottom-0 flex w-full h-[70px] border-t border-t-gray-700 bg-black"
+        >
+            <div class="flex w-full max-w-[500px] mx-auto items-center justify-around">
+                <button
+                    @click="navigateTo('/')"
+                    class="w-full h-full"
+                >
+                    <Icon name="material-symbols:home-outline" size="35" class="text-white"/>
+                </button>
+                <button
+                    @click="userStore.isMenuOverlay = true"
+                    class="w-full h-full"
+                >
+                    <Icon name="material-symbols:edit-square-outline-rounded" size="35" class="text-white mb-1"/>
+                </button>
+                <button
+                    @click="userStore.isLogoutOverlay = true"
+                    class="w-full h-full"
+                >
+                    <Icon name="ph:sign-out" size="35" class="text-white mb-1"/>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <MainLayout>
+        <div id="IndexPage" class="w-full overflow-auto">
+            <div class="mx-auto max-w-[500px] overflow-hidden">
+                <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+
+                    <div v-if="isPosts" v-for="post in posts" :key="post">
+                        <Post :post="post" @isDeleted="posts = []" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+<script setup>
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+
+import MainLayout from '~/layouts/MainLayout.vue';
+
+// const client = useSupabaseClient();
+// const user = useSupabaseUser();
+
+let posts = ref([]);
+let isPosts = ref(true);
+let isLoading = ref(false);
+
+onBeforeMount(() => {
+    posts.value = [
+        {
+            name: 'Joe Biden',
+            image: 'https://placehold.co/100',
+            text: 'This is the title',
+            picture: 'https://placehold.co/500',
+        }
+    ]
+})
+</script>
